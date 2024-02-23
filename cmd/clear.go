580343d3653cd9a2bacce5678627ade5ac6edd93
Christian Doucette <cdoucette@uchicago.edu>
2024-02-23 13:06:28 -0500

    Add separate subway and bus commands

--- cmd/clear.go
+++ cmd/clear.go
@@ -9,15 +9,18 @@ import (
 
 // clearCmd represents the clear command
 var clearCmd = &cobra.Command{
-	Use:   "clear [--bus YOUR_I2C_BUS]",
+	Use:   "clear [--i2c-bus YOUR_I2C_BUS]",
 	Short: "Clears display for OLED monitor",
 	Long:  "This command clears the display for an OLED monitor connected via I2C.",
 	Run: func(cmd *cobra.Command, args []string) {
-		bus, _ := cmd.Flags().GetInt("bus")
-		oled.ClearDisplay(fmt.Sprint(bus))
+		i2cBus, _ := cmd.Flags().GetInt("i2c-bus")
+		oled.ClearDisplay(fmt.Sprint(i2cBus))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(clearCmd)
+
+	clearCmd.Flags().Int("i2c-bus", 1, "Bus for the I2C connection")
+
 }
--- cmd/root.go
+++ cmd/root.go
@@ -3,44 +3,25 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strings"
 
-	"github.com/christian-doucette/time-to-go/internal/gtfs"
-	"github.com/christian-doucette/time-to-go/internal/mta"
-	"github.com/christian-doucette/time-to-go/internal/oled"
 	"github.com/spf13/cobra"
 )
 
 var (
-	mtaApiKey, stopId string
-	debug             bool
+	mtaApiKey string
+	stopId    string
+	debug     bool
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "./time-to-go --mta-api-key YOUR_MTA_API_KEY --stop-id YOUR_STOP_ID [--bus YOUR_I2C_BUS] [--debug]",
+	Use:   "./time-to-go",
 	Short: "Display next subway times on OLED monitor via I2C",
 	Long: `This command will pull the next subway arrival times for a specific stop from the MTA API. 	
 If the debug option is included, it will print the arrival times to the terminal output.
 If the debug option is not included, it will display the arrival times on an OLED display connected over I2C.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		bus, _ := cmd.Flags().GetInt("bus")
-
-		// gets the subway arrival data right now
-		gtfsRaw := mta.CallAllSubwayRealtimeFeedApis(mtaApiKey)
-
-		// parses out the next 5 arrival times for this stop
-		arrivalTimes := gtfs.ExtractSubwayStopArrivalTimes(gtfsRaw, stopId, 4)
-
-		if debug {
-			// if debug option selected, prints arrival times to stdout
-			fmt.Println(strings.Join(arrivalTimes, "\n"))
-
-		} else {
-			// otherwise displays arrival times to OLED display
-			oled.DisplayTextLines(arrivalTimes, 13, 12, fmt.Sprint(bus))
-		}
-
+		fmt.Println("hi there")
 	},
 }
 
